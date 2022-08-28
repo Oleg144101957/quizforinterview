4b03a2527006aa6f288726363e60f58dd0991b08
OlegV <vishnevskiyoleg@gmail.com>
2022-08-28 15:52:42 +0300

    firebase

--- app/src/main/java/com/example/quizforinterview/ResultActivity.kt
+++ app/src/main/java/com/example/quizforinterview/ResultActivity.kt
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.quizforinterview.databinding.ActivityResultBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class ResultActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityResultBinding
+    private lateinit var mDatabase: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,12 +19,19 @@ class ResultActivity : AppCompatActivity() {
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
 
-        val userName = intent.getStringExtra(Constants.USER_NAME)
+        val userName = intent.getStringExtra(Constants.USER_NAME) ?: "unknown"
         val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
         val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
-
         binding.tvName.text = userName
         binding.tvScore.text = "Your score is $correctAnswers out of $totalQuestions"
+
+        mDatabase = FirebaseDatabase.getInstance().getReference(userName)
+
+        val id = mDatabase.key
+        val name = userName
+        val info =binding.tvScore.text.toString()
+
+        mDatabase.push().setValue("Game id $id name of user: $name score is: $info")
 
         binding.btnFinish.setOnClickListener{
             startActivity(Intent(this, MainActivity::class.java))
